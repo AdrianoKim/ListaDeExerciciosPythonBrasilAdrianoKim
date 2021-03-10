b389f367f5070fe3453c0f9cb3a648d07ce185bc
kimzerapqp <kimzerapqp@gmail.com>
2021-03-10 15:29:37 -0300

    update dos exercícios 1 ao 8

---
+++ PythonBrasil/1. Estrutura Sequencial/8. horas trabalhadas e salario.py
@@ -0,0 +1,7 @@
+""" 8.Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+Calcule e mostre o total do seu salário no referido mês."""
+
+hr = float(input('Digite o valor ganho por cada hora trabalhada: '))
+qtde = float(input('Digite a quantidade de horas trabalhadas no mês: '))
+
+print('O salário bruto total referente ao mês trabalhado é: ', 'R$', hr * qtde)
