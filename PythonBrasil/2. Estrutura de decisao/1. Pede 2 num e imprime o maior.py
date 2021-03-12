c22725f325eca80cf88f5a07701fb880dad0a688
kimzerapqp <kimzerapqp@gmail.com>
2021-03-12 15:20:52 -0300

    criado exercicio 1

---
+++ PythonBrasil/2. Estrutura de decisao/1. Pede 2 num e imprime o maior.py
@@ -0,0 +1,13 @@
+""" 1.Faça um Programa que peça dois números e imprima o maior deles. """
+
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+
+if n1 > n2:
+    print('O maior número é: ', n1)
+
+elif n1 == n2:
+    print('Os dois numeros são iguais!')
+
+else:
+    print('O maior número é: ', n2)
