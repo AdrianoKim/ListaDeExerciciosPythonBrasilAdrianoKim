9795a4afb55a8d040e6626c3d7d1ad0611a7d750
kimzerapqp <kimzerapqp@gmail.com>
2021-03-12 15:26:24 -0300

    criado exercicio 2

---
+++ PythonBrasil/2. Estrutura de decisao/2. numero positivo ou negativo.py
@@ -0,0 +1,9 @@
+""" 2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo. """
+
+val = float(input('Digite o valor: '))
+
+if val >= 0:
+    print('O valor informado é positivo')
+
+else:
+    print('O valor informado é negativo')
