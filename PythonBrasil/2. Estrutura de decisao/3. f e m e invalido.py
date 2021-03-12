c3c2715daa4d70011203f80a5dab163f6e6470a4
kimzerapqp <kimzerapqp@gmail.com>
2021-03-12 15:53:50 -0300

    criado exercicio 3

---
+++ PythonBrasil/2. Estrutura de decisao/3. f e m e invalido.py
@@ -0,0 +1,13 @@
+""" 3. Faça um Programa que verifique se uma letra digitada
+ é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido."""
+
+sexo = str.upper(input('Digite o sexo: '))
+
+if sexo == 'F':
+    print('F - Feminino')
+
+elif sexo == 'M':
+    print('M - Masculino')
+
+else:
+    print('Sexo Inválido')
