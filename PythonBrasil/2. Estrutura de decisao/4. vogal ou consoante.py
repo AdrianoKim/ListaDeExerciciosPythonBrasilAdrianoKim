70e649969d30e35bb53d3dcf72f2ad190571c453
kimzerapqp <kimzerapqp@gmail.com>
2021-03-12 16:24:06 -0300

    criado exercicio 4 e testes

---
+++ PythonBrasil/2. Estrutura de decisao/4. vogal ou consoante.py
@@ -0,0 +1,10 @@
+""" 4. Faça um Programa que verifique se uma letra digitada é vogal ou consoante. """
+
+letra = str.upper(input('Digite  a letra: '))
+lista = str(list['A', 'E', 'I', 'O', 'U'])
+
+if letra in lista:
+    print('A letra é uma vogal.')
+
+else:
+    print('A letra é uma consoante.')
---
+++ PythonBrasil/2. Estrutura de decisao/testes decisao.py
@@ -0,0 +1,8 @@
+letra = str.upper(input('digite a letra: '))
+lista = str(list['A', 'E', 'I', 'O', 'U'])
+
+if letra in lista:
+    print('vogal')
+
+else:
+    print('consoante')
