c1755789075a93c0021c128bbf751b671fb84f17
kimzerapqp <kimzerapqp@gmail.com>
2021-03-15 15:44:19 -0300

    criado exercicios 9

---
+++ PythonBrasil/2. Estrutura de decisao/9. numeros em ordem crescente.py
@@ -0,0 +1,10 @@
+""" 9. Faça um Programa que leia três números e mostre-os em ordem decrescente. """
+
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+n3 = float(input('Digite o terceiro número: '))
+
+lista = [n1, n2, n3]
+lista2 = sorted(lista, reverse=True)
+
+print('Os números em ordem decrescente: ', lista2)
