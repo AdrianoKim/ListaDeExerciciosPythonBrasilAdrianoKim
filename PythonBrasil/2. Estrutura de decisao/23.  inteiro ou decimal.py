4dc923f05e8a36b9013702766250afe3e84cbaa8
kimzerapqp <kimzerapqp@gmail.com>
2021-03-19 16:00:13 -0300

    criado exercicio 23

---
+++ PythonBrasil/2. Estrutura de decisao/23.  inteiro ou decimal.py
@@ -0,0 +1,11 @@
+""" 23. Faça um Programa que peça um número e informe se o número é inteiro ou decimal.
+Dica: utilize uma função de arredondamento.
+"""
+
+num = float(input('Digite o número: '))
+
+if num != round(num):
+    print('O número é decimal')
+
+else:
+    print('O número é inteiro.')
