88655d6ffc9275fcc5f4f2a3491d56e1c57e413f
kimzerapqp <kimzerapqp@gmail.com>
2021-04-08 15:07:04 -0300

    criado exercicio 1

--- PythonBrasil/3. Estrutura de repeticao/testes.py
+++
@@ -1,6 +0,0 @@
-thislist = ["apple", "banana", "cherry", "coco"]
-myorder = "I want to buy {}, {} and {}."
-print(len(thislist))
-print(thislist[0:2])
-print(myorder.format(thislist[0], thislist[1], thislist[2]))
-
